08f1827399fc238f1df5c93452435f5c27911aa9
matheusBBarni <matheus@demacode.com.br>
2020-12-13 14:51:36 -0300

    wip

---
+++ SistemaLojaQualquer/src/model/Produto.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Produto {
+
+	private Integer idProduto;
+	private String nome;
+	private String descricao;
+	private String codigo;
+	private Integer quantidade;
+
+	public Produto() {}
+
+	public Integer getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Integer idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
+
